ea86715f8ce12386cad047e7bb716ab200cc31c3
Asrumochan <asrumochanparida99@gmail.com>
2023-09-16 00:49:24 +0530

    sasdc

--- numbers_in_string.js
+++ numbers_in_string.js
@@ -2,9 +2,9 @@ const check=function (arr) {
     for (let i = 0; i< arr.length; i++) {
         res= /[0-9]/.test(arr[i]);
         if(res==true)
-        return arr[i]+" has a number";
+        return arr[i]+" has a number.";
     }
 }
-arr=["das2d","a3d","12b"];
+arr=["dasd","a3d","12b"];
 var res="";
 console.log(check(arr));
